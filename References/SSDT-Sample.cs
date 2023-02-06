a3150cfdefacfb4334eb7392fe474fcc584dea92
copypaste <72515939+theofficialcopypaste@users.noreply.github.com>
2023-02-07 00:44:43 +0800

    Add files via upload
    Sample of my SSDT

---
+++ References/SSDT-Sample.cs
@@ -0,0 +1,109 @@
+DefinitionBlock("", "SSDT", 2, "MSI", "B460", 0x00002000) {
+	External(_SB_.PCI0, DeviceObj)
+	External(_SB_.PCI0.LPCB, DeviceObj)
+	External(_SB_.PCI0.PEG0, DeviceObj)
+	External(_SB_.PCI0.PEG0.PEGP, DeviceObj)
+	External(_SB_.PCI0.SBUS, DeviceObj)
+	External(STAS, IntObj)
+
+	Scope(\_SB) {
+		If(_OSI("Darwin")) {
+			Method(_INI, 0, NotSerialized) // _INI: Initialize
+			{
+				STAS = One
+			}
+		}
+
+		If(_OSI("Darwin")) {
+			Scope(PCI0) {
+				Device(DRAM) {
+					Name(_ADR, Zero) // _ADR: Address
+				}
+
+				Scope(LPCB) {
+					Device(EC) {
+						Name(_HID, "ACID0001") // _HID: Hardware ID
+						Method(_STA, 0, NotSerialized) // _STA: Status
+						{
+							Return(0x0F)
+						}
+					}
+				}
+
+				Scope(PEG0) {
+					Scope(PEGP) {
+						Method(_STA, 0, NotSerialized) // _STA: Status
+						{
+							Return(Zero)
+						}
+					}
+
+					Device(EGP0) {
+						Name(_ADR, Zero) // _ADR: Address
+						Device(EGP1) {
+							Name(_ADR, Zero) // _ADR: Address
+						}
+					}
+				}
+
+				Device(PGMM) {
+					Name(_ADR, 0x00080000) // _ADR: Address
+				}
+
+				Scope(SBUS) {
+					Device(BUS0) {
+						Name(_CID, "smbus") // _CID: Compatible ID
+						Name(_ADR, Zero) // _ADR: Address
+						Device(DVL0) {
+							Name(_ADR, 0x57) // _ADR: Address
+							Name(_CID, "diagsvault") // _CID: Compatible ID
+							Method(_DSM, 4, NotSerialized) // _DSM: Device-Specific Method
+							{
+								Return(0x0F)
+							}
+						}
+
+						Method(_STA, 0, NotSerialized) // _STA: Status
+						{
+							Return(0x0F)
+						}
+					}
+				}
+
+				Device(THSS) {
+					Name(_ADR, 0x00140002) // _ADR: Address
+				}
+			}
+		}
+
+		If(_OSI("Darwin")) {
+			Device(USBX) {
+				Name(_ADR, Zero) // _ADR: Address
+				Method(_DSM, 4, NotSerialized) // _DSM: Device-Specific Method
+				{
+					If((Arg2 == Zero)) {
+						Return(Buffer(One) {
+							0x03 // .
+						})
+					}
+
+					Return(Package(0x08) {
+						"kUSBSleepPowerSupply",
+						0x13EC,
+						"kUSBSleepPortCurrentLimit",
+						0x0834,
+						"kUSBWakePowerSupply",
+						0x13EC,
+						"kUSBWakePortCurrentLimit",
+						0x0834
+					})
+				}
+
+				Method(_STA, 0, NotSerialized) // _STA: Status
+				{
+					Return(0x0F)
+				}
+			}
+		}
+	}
+}
